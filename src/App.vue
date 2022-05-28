fff93ccf0e35b60c7b670d46ef8884480b9da157
abing123456 <2990904890@qq.com>
2022-05-28 18:53:31 +0800

    main

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,21 @@
 <template>
+  <div id="app">
+
+  </div>
 </template>
 
 <script>
 
-export default {}
+export default {
+  name: 'app',
+  components: {
+
+  }
+}
   
 
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import "./assets/css/base.css";
 </style>
