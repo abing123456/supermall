e7802adbb8cd05188ef1cf9864645e7d1e53116b
abing123456 <2990904890@qq.com>
2022-05-29 16:04:09 +0800

    mmm

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
 </template>
 
 <script>
-import MainTabBarVue from 'components/content/MainTabBar.vue'
+import MainTabBarVue from 'components/content/MainTabBar/MainTabBar'
 
 export default {
   name: 'app',
--- src/components/content/MainTabBar/MainTabBar.vue
+++ src/components/content/MainTabBar/MainTabBar.vue
@@ -1,23 +1,23 @@
 <template>
   <tab-bar>
      <tab-bar-item path="/home" activeColor="blue">
-        <img slot="item-icon" src="~assets/img/icon/shouye.png" alt="">
-        <img slot="item-icon-active" src="~assets/img/icon/shouye-active.png" alt="">
+        <img slot="item-icon" src="@assets/img/tabbar/home.svg" alt="">
+        <img slot="item-icon-active" src="@assets/img/icon/shouye-active.png" alt="">
         <div slot="item-text">首页</div>
      </tab-bar-item>
       <tab-bar-item path="/category" activeColor="blue">
-        <img slot="item-icon" src="~assets/img/icon/fenlei.png" alt="">
-        <img slot="item-icon-active" src="~assets/img/icon/fenlei-active.png" alt="">
+        <img slot="item-icon" src="@assets/img/icon/fenlei.png" alt="">
+        <img slot="item-icon-active" src="@assets/img/icon/fenlei-active.png" alt="">
         <div slot="item-text">分类</div>
        </tab-bar-item>
       <tab-bar-item path="/cart" activeColor="blue">
-        <img slot="item-icon" src="~assets/img/icon/gouwuche.png" alt="">
-        <img slot="item-icon-active" src="~assets/img/icon/gouwuche-active.png" alt="">
+        <img slot="item-icon" src="@assets/img/icon/gouwuche.png" alt="">
+        <img slot="item-icon-active" src="@assets/img/icon/gouwuche-active.png" alt="">
         <div slot="item-text">购物车</div>
       </tab-bar-item>
       <tab-bar-item path="/profile" activeColor="blue">
-        <img slot="item-icon" src="~assets/img/icon/wode.png" alt="">
-        <img slot="item-icon-active" src="~assets/img/icon/wode-active.png" alt="">
+        <img slot="item-icon" src="@assets/img/icon/wode.png" alt="">
+        <img slot="item-icon-active" src="@assets/img/icon/wode-active.png" alt="">
         <div slot="item-text">我的</div>
       </tab-bar-item>
     </tab-bar>
